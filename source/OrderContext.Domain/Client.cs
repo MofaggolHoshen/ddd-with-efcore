1ca946926d307054d336890e66f13d184ddf6578
Mofaggol Hoshen <Mofaggol.Hoshen@outlook.com>
2026-04-09 23:57:25 +0200

    Enhance Client class with additional constructors and update methods for name and email

--- source/OrderContext.Domain/Client.cs
+++ source/OrderContext.Domain/Client.cs
@@ -21,6 +21,13 @@ public class Client
     {
 
     }
+
+    /// <summary>
+    /// Private constructor for internal use. Use the static Create method to instantiate a new Client.
+    /// </summary>
+    /// <param name="id">The unique identifier of the client.</param>
+    /// <param name="name">The name of the client.</param>
+    /// <param name="email">The email of the client.</param>
     private Client(Guid id, string name, Email email)
     {
         Id = id;
@@ -29,11 +36,24 @@ public class Client
         CreatedAt = DateTime.UtcNow;
     }
 
+    /// <summary>
+    /// Private constructor for internal use. Use the static Create method to instantiate a new Client.
+    /// </summary>
+    /// <param name="name">The name of the client.</param>
+    /// <param name="email">The email of the client.</param>
     private Client(string name, Email email)
         : this(Guid.NewGuid(), name, email)
     {
     }
 
+    /// <summary>
+    /// Static factory method to create a new Client instance. Validates the input parameters and throws exceptions if they are invalid.
+    /// </summary>
+    /// <param name="name">The name of the client.</param>
+    /// <param name="email">The email of the client.</param>
+    /// <returns>A new instance of the Client class.</returns>
+    /// <exception cref="ArgumentException">Thrown when the name is null or whitespace.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when the email is null.</exception>
     public static Client Create(string name, Email email)
     {
         // Validate the client's name
@@ -47,7 +67,11 @@ public class Client
 
     }
 
-
+    /// <summary>
+    /// Updates the client's name. Validates the new name and throws an exception if it is invalid.
+    /// </summary>
+    /// <param name="newName">The new name of the client.</param>
+    /// <exception cref="ArgumentException">Thrown when the new name is null or whitespace.</exception>
     public void UpdateName(string newName)
     {
         if (string.IsNullOrWhiteSpace(newName))
@@ -55,6 +79,11 @@ public class Client
         Name = newName;
     }
 
+    /// <summary>
+    /// Updates the client's email. Validates the new email and throws an exception if it is null.
+    /// </summary>
+    /// <param name="newEmail">The new email of the client.</param>
+    /// <exception cref="ArgumentNullException">Thrown when the new email is null.</exception>
     public void UpdateEmail(Email newEmail)
     {
         Email = newEmail ?? throw new ArgumentNullException(nameof(newEmail));
